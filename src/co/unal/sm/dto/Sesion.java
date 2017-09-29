c738b1716f75f5809ae8efde7b68f94907cddb55
Unknown <jmhurtadoc@unal.edu.co>
2017-09-29 01:18:55 -0500

    Token

---
+++ src/co/unal/sm/dto/Sesion.java
@@ -0,0 +1,24 @@
+package co.unal.sm.dto;
+
+public class Sesion {
+
+	private Integer id;
+	private String token;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
